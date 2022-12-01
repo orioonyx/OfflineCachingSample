e8d4507927d0a77fd0508744893792670c7515eb
KyungEun No <74607521+shruddms@users.noreply.github.com>
2022-12-01 23:33:06 +0900

    Feat: Add timber

---
+++ app/src/main/java/com/kyungeun/offlinecachingsample/MainApplication.kt
@@ -0,0 +1,18 @@
+package com.kyungeun.offlinecachingsample
+
+import android.app.Application
+import androidx.viewbinding.BuildConfig
+import dagger.hilt.android.HiltAndroidApp
+import timber.log.Timber
+
+@HiltAndroidApp
+class MainApplication : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+        if (BuildConfig.DEBUG) {
+            Timber.plant(Timber.DebugTree())
+        }
+    }
+
+}
